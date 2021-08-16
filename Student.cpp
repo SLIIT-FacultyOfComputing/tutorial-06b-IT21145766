772e7e3b0b427c58063ffb74b5244c5ea8ee5ce0
IT21145766 <87365617+IT21145766@users.noreply.github.com>
2021-08-16 13:35:37 +0000

    All tutes completed

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,20 @@
 #include "Student.h"
 #include <iostream>
+using namespace std;
+#include<cstring>
 
 // Assign studentId and name
-Student::assignDetails() {
+void Student::assignDetails(int pstudentId, char pname[]) {
   
+  studentId=pstudentId;
+  strcpy(name,pname);
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
+
+  cout<<"Student ID: "<<studentId <<endl;
+  cout<<"Student name: "<<name <<endl;
+  
   
 }
